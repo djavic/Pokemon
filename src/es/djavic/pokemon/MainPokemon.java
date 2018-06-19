14c3075c4f143b37e35595c1ebc1f9eff2ab2052
falvad01 <falvad01@estudiantes.unileon.es>
2018-06-19 10:28:06 +0200

    Primer commit, cracion de clases provisionales

---
+++ src/es/djavic/pokemon/MainPokemon.java
@@ -0,0 +1,10 @@
+package es.djavic.pokemon;
+
+public class MainPokemon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/es/djavic/pokemon/Player.java
@@ -0,0 +1,5 @@
+package es.djavic.pokemon;
+
+public class Player {
+
+}
